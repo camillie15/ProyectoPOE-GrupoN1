c928ab7674fdad78b4ecd5471e3b187eb8e9fca9
camillie15 <138990540+camillie15@users.noreply.github.com>
2024-05-14 23:10:31 -0500

    Clases Modelo: Definicion Administrador, Menu, MenuItem y Usuario

---
+++ Modelo/Administrador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    //By: Erick Cordova Viteri
+    public class Administrador : Usuario
+    {
+        int idAdministrador = 0;
+        public int IdAdministrador { get => idAdministrador; set => idAdministrador = value; }
+
+        public Administrador(string nombre, string apellido, string cedula, int edad, string email, string userName, string password, bool estado, int idAdministrador) : base(nombre, apellido, cedula, edad, email, userName, password, estado)
+        {
+            this.idAdministrador = idAdministrador;
+        }
+
+        public string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Modelo/MenuItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class MenuItem
+    {
+        private int idMenu;
+        private string nombre;
+        private string descripcion;
+        private double precio;
+        private bool estado;
+
+        public MenuItem(int idMenu, string nombre, string descripcion, double precio, bool estado)
+        {
+            this.idMenu = idMenu;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            this.estado = estado;
+        }
+        public int IdMenu { get => idMenu; set => idMenu = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public double Precio { get => precio; set => precio = value; }
+        public bool Estado { get => estado; set => estado = value; }
+
+        public override string ToString()
+        {
+            return "Nombre: " + nombre + "\nDescripción: " + descripcion + "\nPrecio: " + precio + "\nEstado: " + (estado ? "Activo" : "Inactivo");
+        }
+    }
+}
