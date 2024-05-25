922183eb98e2eec283beeee4233de3c39db4958a
ErickCor24 <erick.cordovavit@ug.edu.ec>
2024-05-25 15:57:33 -0500

    Agregar codigo en el main (program.cs) para la inicializacion de la ventana

--- Vista/IngresarCliente.Designer.cs
+++ Vista/IngresarCliente.Designer.cs
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblIdCliente = new System.Windows.Forms.Label();
+            this.lblNombreCliente = new System.Windows.Forms.Label();
+            this.txtIdCliente = new System.Windows.Forms.TextBox();
+            this.txtNombreCliente = new System.Windows.Forms.TextBox();
+            this.lblApellidoCliente = new System.Windows.Forms.Label();
+            this.txtApellidoCliente = new System.Windows.Forms.TextBox();
+            this.lblCedulaCliente = new System.Windows.Forms.Label();
+            this.txtCedulaCliente = new System.Windows.Forms.TextBox();
+            this.lblEmailCliente = new System.Windows.Forms.Label();
+            this.txtEmailCliente = new System.Windows.Forms.TextBox();
+            this.btnContinuarCliente = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblIdCliente
+            // 
+            this.lblIdCliente.AutoSize = true;
+            this.lblIdCliente.Location = new System.Drawing.Point(25, 32);
+            this.lblIdCliente.Name = "lblIdCliente";
+            this.lblIdCliente.Size = new System.Drawing.Size(53, 13);
+            this.lblIdCliente.TabIndex = 0;
+            this.lblIdCliente.Text = "ID Cliente";
+            // 
+            // lblNombreCliente
+            // 
+            this.lblNombreCliente.AutoSize = true;
+            this.lblNombreCliente.Location = new System.Drawing.Point(25, 68);
+            this.lblNombreCliente.Name = "lblNombreCliente";
+            this.lblNombreCliente.Size = new System.Drawing.Size(47, 13);
+            this.lblNombreCliente.TabIndex = 1;
+            this.lblNombreCliente.Text = "Nombre:";
+            // 
+            // txtIdCliente
+            // 
+            this.txtIdCliente.Location = new System.Drawing.Point(84, 29);
+            this.txtIdCliente.Name = "txtIdCliente";
+            this.txtIdCliente.Size = new System.Drawing.Size(97, 20);
+            this.txtIdCliente.TabIndex = 2;
+            // 
+            // txtNombreCliente
+            // 
+            this.txtNombreCliente.Location = new System.Drawing.Point(78, 65);
+            this.txtNombreCliente.Name = "txtNombreCliente";
+            this.txtNombreCliente.Size = new System.Drawing.Size(314, 20);
+            this.txtNombreCliente.TabIndex = 3;
+            // 
+            // lblApellidoCliente
+            // 
+            this.lblApellidoCliente.AutoSize = true;
+            this.lblApellidoCliente.Location = new System.Drawing.Point(25, 106);
+            this.lblApellidoCliente.Name = "lblApellidoCliente";
+            this.lblApellidoCliente.Size = new System.Drawing.Size(47, 13);
+            this.lblApellidoCliente.TabIndex = 4;
+            this.lblApellidoCliente.Text = "Apellido:";
+            // 
+            // txtApellidoCliente
+            // 
+            this.txtApellidoCliente.Location = new System.Drawing.Point(81, 103);
+            this.txtApellidoCliente.Name = "txtApellidoCliente";
+            this.txtApellidoCliente.Size = new System.Drawing.Size(311, 20);
+            this.txtApellidoCliente.TabIndex = 5;
+            // 
+            // lblCedulaCliente
+            // 
+            this.lblCedulaCliente.AutoSize = true;
+            this.lblCedulaCliente.Location = new System.Drawing.Point(25, 135);
+            this.lblCedulaCliente.Name = "lblCedulaCliente";
+            this.lblCedulaCliente.Size = new System.Drawing.Size(43, 13);
+            this.lblCedulaCliente.TabIndex = 6;
+            this.lblCedulaCliente.Text = "Cedula:";
+            // 
+            // txtCedulaCliente
+            // 
+            this.txtCedulaCliente.Location = new System.Drawing.Point(28, 151);
+            this.txtCedulaCliente.Name = "txtCedulaCliente";
+            this.txtCedulaCliente.Size = new System.Drawing.Size(214, 20);
+            this.txtCedulaCliente.TabIndex = 7;
+            // 
+            // lblEmailCliente
+            // 
+            this.lblEmailCliente.AutoSize = true;
+            this.lblEmailCliente.Location = new System.Drawing.Point(250, 135);
+            this.lblEmailCliente.Name = "lblEmailCliente";
+            this.lblEmailCliente.Size = new System.Drawing.Size(35, 13);
+            this.lblEmailCliente.TabIndex = 8;
+            this.lblEmailCliente.Text = "Email:";
+            // 
+            // txtEmailCliente
+            // 
+            this.txtEmailCliente.Location = new System.Drawing.Point(253, 151);
+            this.txtEmailCliente.Name = "txtEmailCliente";
+            this.txtEmailCliente.Size = new System.Drawing.Size(214, 20);
+            this.txtEmailCliente.TabIndex = 9;
+            // 
+            // btnContinuarCliente
+            // 
+            this.btnContinuarCliente.Location = new System.Drawing.Point(260, 264);
+            this.btnContinuarCliente.Name = "btnContinuarCliente";
+            this.btnContinuarCliente.Size = new System.Drawing.Size(75, 23);
+            this.btnContinuarCliente.TabIndex = 10;
+            this.btnContinuarCliente.Text = "Continuar";
+            this.btnContinuarCliente.UseVisualStyleBackColor = true;
+            // 
+            // IngresarCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(624, 321);
+            this.Controls.Add(this.btnContinuarCliente);
+            this.Controls.Add(this.txtEmailCliente);
+            this.Controls.Add(this.lblEmailCliente);
+            this.Controls.Add(this.txtCedulaCliente);
+            this.Controls.Add(this.lblCedulaCliente);
+            this.Controls.Add(this.txtApellidoCliente);
+            this.Controls.Add(this.lblApellidoCliente);
+            this.Controls.Add(this.txtNombreCliente);
+            this.Controls.Add(this.txtIdCliente);
+            this.Controls.Add(this.lblNombreCliente);
+            this.Controls.Add(this.lblIdCliente);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "IngresarCliente";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblIdCliente;
+        private System.Windows.Forms.Label lblNombreCliente;
+        private System.Windows.Forms.TextBox txtIdCliente;
+        private System.Windows.Forms.TextBox txtNombreCliente;
+        private System.Windows.Forms.Label lblApellidoCliente;
+        private System.Windows.Forms.TextBox txtApellidoCliente;
+        private System.Windows.Forms.Label lblCedulaCliente;
+        private System.Windows.Forms.TextBox txtCedulaCliente;
+        private System.Windows.Forms.Label lblEmailCliente;
+        private System.Windows.Forms.TextBox txtEmailCliente;
+        private System.Windows.Forms.Button btnContinuarCliente;
     }
 }
