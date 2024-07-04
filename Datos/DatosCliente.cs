01c865eccee0ae681d9559f6b746bc565e8392d4
ErickCor24 <erick.cordovavit@ug.edu.ec>
2024-07-03 22:44:55 -0500

    Creacion de datosCliente y metodo para ingresra los datos dentro de SQL

--- Datos/DatosCliente.cs
+++ Datos/DatosCliente.cs
@@ -10,9 +10,40 @@ namespace Datos
 {
     public class DatosCliente
     {
-        public string IngresarCliente(Cliente cliente, SqlConnection cn)
+        Conexion cn = new Conexion();
+        SqlCommand cmd = new SqlCommand();
+        public string IngresarCliente(Cliente cliente)
         {
-            return "";
+            char estado = ComprobarEstado(cliente.Estado);
+            string comando = $"INSERT INTO Persona (nombre, apellido, cedula, edad, email, estado) VALUES ('{cliente.Nombre}'," +
+                $"'{cliente.apellido}', '{cliente.Cedula}', {cliente.Edad} , '{cliente.Email}', '{estado}') ";
+            try
+            {
+                cn.Conectar();
+                cmd.Connection = cn.Cn;
+                cmd.CommandText = comando;
+                cmd.ExecuteNonQuery();
+                cn.Desconectar();
+                return "Se pudo registrar";
+            }
+            catch (SqlException ex)
+            {
+                return $"No se pudo registrar {ex}";
+            }
+        }
+
+        private char ComprobarEstado(bool flag)
+        {
+            char result = ' ';
+            if (flag == true)
+            {
+                result = '1';
+            }
+            else
+            {
+                result = '0';
+            }
+            return result;
         }
     }
 }
